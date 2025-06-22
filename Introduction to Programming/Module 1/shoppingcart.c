3f6553f4b50b486136d65fcbebded4d804c3f610
mdshahriarkabiromi <shahriar.eu.cse59@gmail.com>
2025-06-22 22:57:09 +0600

    Added shoppingcart

---
+++ Introduction to Programming/Module 1/shoppingcart.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    char item[30];
+    float price;
+    int amount;
+    printf("What item would you like to buy?: ");
+    scanf("%s",&item);
+    printf("What is the price for each?:");
+    scanf("%f",&price);
+    printf("How many would you like?:");
+    scanf("%d",&amount);
+    printf("\n");
+    printf("You have brought %d pizza\n",amount);
+    printf("The total is : %.2f\n",price*amount);
+
+
+
+    return 0;
+}
